d0a70f87fae43153bdab1de8fa33ef1f61933296
kot2202 <33962827+kot2202@users.noreply.github.com>
2021-12-05 15:41:52 +0100

    * Created basic placeholder looping menu for future crud operations * Created console title changing support to the settings * Added color changing to console (may be troublesome https://stackoverflow.com/a/34866857 )

--- Library_Project/Program.cs
+++ Library_Project/Program.cs
@@ -10,6 +10,43 @@ namespace Library_Project
     {
         static void Main(string[] args)
         {
+            ////////////////// SETTINGS //////////////////
+
+            Console.ForegroundColor = ConsoleColor.Red; // TODO: https://stackoverflow.com/a/34866857
+
+            Console.Title = "Library Project";
+            const string menuLoopTextHead = "Wybierz opcję i zatwierdź przy użyciu <ENTER>\n";
+            const string menuLoopTextBody = "1. Wyświetl stan książek\n2. Wypożycz książkę\n3. Dodaj książkę\n4. Usuń książkę";
+
+            //////////////////////////////////////////////
+
+            Console.OutputEncoding = Encoding.UTF8; // Allows the usage of polish and other characters
+
+            //////////////////////////////////////////////
+
+            ushort wybor;
+
+            while(true)
+            {
+                Console.WriteLine(menuLoopTextHead + menuLoopTextBody);
+
+                //wybor = Convert.ToUInt16(Console.ReadLine()); // wysypuje sie przy wpisaniu tekstu zamiast liczb
+                ushort.TryParse(Console.ReadLine(), out wybor); // nie wysypuje sie, zwraca 0 po wpisaniu tekstu
+
+                switch (wybor)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
