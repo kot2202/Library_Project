4fa29a4d211c63363c7ed9bad78a3a14044d4d36
kot2202 <kot2202@onet.pl>
2022-01-12 19:44:24 +0100

    Updated Main program
    ** Altered settings a bit, printed messages should be preceded by their respected color variant
    - Altered string structure a bit
    * Added not fully implemented solutions to assignment 1,2

--- Library_Project/Program.cs
+++ Library_Project/Program.cs
@@ -10,24 +10,48 @@ namespace Library_Project
     {
         static void Main(string[] args)
         {
+
+            #region settings
             ////////////////// SETTINGS //////////////////
 
-            Console.ForegroundColor = ConsoleColor.Red; // TODO: https://stackoverflow.com/a/34866857
+            /// General Colors ///
+            ConsoleColor generalTextColor = ConsoleColor.White;
+            ConsoleColor errorTextColor = ConsoleColor.Red;
+            ConsoleColor questionTextColor = ConsoleColor.Yellow;
+            ConsoleColor informationTextColor = ConsoleColor.Green;
+            Console.ForegroundColor = generalTextColor; // TODO: https://stackoverflow.com/a/34866857
 
             Console.Title = "Library Project";
             const string menuLoopTextHead = "Wybierz opcję i zatwierdź przy użyciu <ENTER>\n";
-            const string menuLoopTextBody = "1. Wyświetl stan książek\n2. Wypożycz książkę\n3. Dodaj książkę\n4. Usuń książkę";
+            const string menuLoopTextBody = "1. Wyświetl stan książek\n" +
+                                            "2. Wypożycz książkę\n" +
+                                            "3. Dodaj książkę\n" +
+                                            "4. Usuń książkę";
+
+            /// Show Books Menu///
+            ConsoleColor menuShowBookTextColor = informationTextColor;
+            const string menuShowBookHead = "Dostępne książki:";
+
+            /// Rent Book Menu///
+            const string menuBookRentBody = "Podaj część tytułu książki, jeśli jest dostępna, zostanie Ci wypożyczona";
 
             //////////////////////////////////////////////
+            #endregion
 
             Console.OutputEncoding = Encoding.UTF8; // Allows the usage of polish and other characters
 
             //////////////////////////////////////////////
 
+            LibraryRepository libraryRepo = new LibraryRepository();
             ushort wybor;
+            string komenda;
 
-            while(true)
+            List<data.Ksiazki> currentlyAvailableBooks = new List<data.Ksiazki>();
+            //library_projectEntities lb = new library_projectEntities();
+
+            while (true)
             {
+                Console.ForegroundColor = generalTextColor;
                 Console.WriteLine(menuLoopTextHead + menuLoopTextBody);
 
                 //wybor = Convert.ToUInt16(Console.ReadLine()); // wysypuje sie przy wpisaniu tekstu zamiast liczb
@@ -36,8 +60,56 @@ namespace Library_Project
                 switch (wybor)
                 {
                     case 1:
+                        //currentlyAvailableBooks = libraryRepo.GetBooks();
+                        //for(int i = 0;i < currentlyAvailableBooks.Count;i++)
+                        //{
+                        //    Console.WriteLine($"{currentlyAvailableBooks[i].nazwa} {currentlyAvailableBooks[i].autor_imie} {currentlyAvailableBooks[i].autor_nazwisko} {currentlyAvailableBooks[i].ilosc_ksiazek}" ); 
+                        //}
+                        //Console.WriteLine(Environment.NewLine);
+
+                        Console.WriteLine(menuShowBookHead);
+                        Console.ForegroundColor = menuShowBookTextColor;
+                        libraryRepo.ShowBooks();
+                        Console.ForegroundColor = generalTextColor;
+
                         break;
                     case 2:
+                        Console.WriteLine(menuBookRentBody);
+                        komenda = Console.ReadLine();
+                        var ksiazki = libraryRepo.GetBooksWithName(komenda);
+                        int nrKsiazki;
+                        if (ksiazki.Count == 0)
+                        {
+                            Console.ForegroundColor = errorTextColor;
+                            Console.WriteLine("Nie znaleziono książek o takiej nazwie");
+                            Console.ForegroundColor = generalTextColor;
+                            break;
+                        }
+                        for (nrKsiazki = 0; nrKsiazki < ksiazki.Count; nrKsiazki++)
+                        {
+                            Console.ForegroundColor = questionTextColor;
+                            Console.WriteLine($"Czy chodzi o {ksiazki[nrKsiazki].nazwa} {ksiazki[nrKsiazki].autor_imie} {ksiazki[nrKsiazki].autor_nazwisko}?\nT/N");
+                            komenda = Console.ReadLine();
+                            Console.ForegroundColor = generalTextColor; // TODO sprawdzic czy da sie zbugowac kolor
+                            if (String.Equals(komenda, "t", StringComparison.OrdinalIgnoreCase)) // pozwala na wpisywanie duzych i malych liter
+                            {
+                                if (ksiazki[nrKsiazki].ilosc_ksiazek > 0)
+                                {
+                                    libraryRepo.ReduceBook(ksiazki[nrKsiazki].id_ksiazki);
+                                    Console.ForegroundColor = informationTextColor;
+                                    Console.WriteLine($"Czytelnik ID Wypożyczył {ksiazki[nrKsiazki].nazwa} {ksiazki[nrKsiazki].autor_imie} {ksiazki[nrKsiazki].autor_nazwisko}"); // TODO dodac wpisywanie id czytelnika przed wypozyczeniem
+                                    goto default;
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = errorTextColor;
+                                    Console.WriteLine("Niestety na stanie jest 0 tych książek");
+                                    goto default;
+                                }
+                            }
+                        }
+                        Console.ForegroundColor = errorTextColor;
+                        Console.WriteLine("Nie ma na stanie więcej książek o tej nazwie");
                         break;
                     case 3:
                         break;
